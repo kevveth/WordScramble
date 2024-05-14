fcb15195ae08c686371cfa7fd325fb332f42aa42
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-05-14 01:00:36 -0700

    Updated to improve accessibility

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -40,6 +40,8 @@ struct ContentView: View {
                             Image(systemName: "\(word.count).circle")
                             Text(word)
                         }
+                        .accessibilityElement()
+                        .accessibilityLabel("\(word), \(word.count) letters")
                     }
                 }
             }
